a7422138cce2a01ef9b5c47bcb183cf4be498ad6
periebm <periclesebmendes@gmail.com>
2024-05-11 19:24:54 -0300

    01 01

--- module-1-javascript-testing/01-working-with-mocks/index.test.js
+++ module-1-javascript-testing/01-working-with-mocks/index.test.js
@@ -1,8 +1,8 @@
 const File = require('./src/file');
 const { error } = require('./src/constants');
-const assert = require('assert')
+const assert = require('assert');
 //IFEE
-;(async () => {
+(async () => {
   //created variables creted in this section are only valid on it's execution
   {
     const filePath = './mocks/emptyFile.invalid.csv';
@@ -10,4 +10,34 @@ const assert = require('assert')
     const result = File.csvToJSON(filePath);
     await assert.rejects(result, expected);
   }
-})()
+
+  {
+    const filePath = './mocks/header.invalid.csv';
+    const expected = new Error(error.FILE_FIELDS_ERROR_MESSAGE);
+    const result = File.csvToJSON(filePath);
+    await assert.rejects(result, expected);
+  }
+
+  {
+    const filePath = './mocks/fiveItens.invalid.csv';
+    const expected = new Error(error.FILE_LENGHT_ERROR_MESSAGE);
+    const result = File.csvToJSON(filePath);
+    await assert.rejects(result, expected);
+  }
+
+  {
+    const filePath = './mocks/threeItems.valid.csv';
+    const expected = [
+      {
+        id: '1',
+        name: 'peri',
+        profession: 'developer',
+        age: '25',
+      },
+      { id: '2', name: 'joaquim', profession: 'fireman', age: '50' },
+      { id: '3', name: 'adao', profession: 'manager', age: '64' },
+    ];
+    const result = await File.csvToJSON(filePath);
+    assert.deepEqual(result, expected);
+  }
+})();
--- module-1-javascript-testing/01-working-with-mocks/src/file.js
+++ module-1-javascript-testing/01-working-with-mocks/src/file.js
@@ -1,34 +1,65 @@
 //everything needed to read files
-const { error } = require('console');
+const { error } = require('./constants');
 const { readFile } = require('fs/promises');
 const DEFAULT_OPTION = {
   maxLines: 3,
-  fields: ['id', 'name', 'profession', 'age']
-}
+  fields: ['id', 'name', 'profession', 'age'],
+};
 
 class File {
   static async csvToJSON(filePath) {
     const content = await readFile(filePath, 'utf8');
-    this.isValid(content);
+    const validation = this.isValid(content);
+    if (!validation.valid) throw new Error(validation.error);
+
+    const result = this.parseCSVToJSON(content);
+    return result;
   }
 
   static isValid(csvString, options = DEFAULT_OPTION) {
     //to see file content:
     //fs.readFileSync('./mocks/threeItems.valid.csv', 'utf8')
 
-    //[0] header 
+    //[0] header
     //[1] first row
     // [2] second row
     // ...variable = rest of file
-    const [headers, ...fileWithoutHeader] = csvString.split(/\r?\n/);
+    const [header, ...fileWithoutHeader] = csvString.split(/\r?\n/);
+    const isHeaderValid = header === options.fields.join(',');
 
-    if(!fileWithoutHeader.length) {
+    if (!isHeaderValid) {
+      return {
+        error: error.FILE_FIELDS_ERROR_MESSAGE,
+        valid: false,
+      };
+    }
+    if (
+      !fileWithoutHeader.length ||
+      fileWithoutHeader.length > options.maxLines
+    ) {
       return {
         error: error.FILE_LENGHT_ERROR_MESSAGE,
-        valid: false
-      }
+        valid: false,
+      };
     }
-    console.log(headers, fileWithoutHeader);
+    return { valid: true };
+  }
+
+  static parseCSVToJSON(csvString) {
+    const lines = csvString.split(/\r?\n/);
+    //remove first line (header)
+    const firstLine = lines.shift();
+    const header = firstLine.split(',');
+
+    const users = lines.map((line) => {
+      const columns = line.split(',');
+      let user = {};
+      for (const index in columns) {
+        user[header[index]] = columns[index].trim();
+      }
+      return user;
+    });
+    return users;
   }
 }
 
